c5643962c4952b9e8fc292009fdd30a046273905
jamesguillen <jguillen.ep>
2016-01-22 22:26:35 +0000

    message about changes

--- todolist.js
+++ todolist.js
@@ -1,27 +1,23 @@
 var todo = document.getElementById('todolist'),
     form = document.getElementById('myform'),
     field = document.getElementById('newitem');
-//This code writes down the item you need and saves it in you list
+
+//This code writes down the item you need and saves it in your list
 form.addEventListener( 'submit', function(evt) {
   var text = field.value;
   todo.innerHTML += '<div class="border">' + text + '</div>';
   evt.preventDefault();
 }, false);
 
-//This allows you to be able to cross out items and delete them from you list
+//This allows you to be able to cross out items and delete them from your list
 todo.addEventListener( 'click', function(evt) {
-  var tar = evt.target;
+   var tar = evt.target;
   if(tar.style.textDecoration === "line-through"){
    tar.parentNode.removeChild(tar);
 }else{
-    tar.style.textDecoration = "line-through"
+    tar.style.textDecoration = "line-through";
 }
   evt.preventDefault();
   
 }, false);
 
-//This adds the color yellow in the text box
-field.style.color = "red";
-if( field.style.color == "red"){
-  field.style.backgroundColor = "yellow";
-}
